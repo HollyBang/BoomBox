5c0d167637601fddeead92e4611fe4b155ca6397
Andrey Barchuk <ndreybar@gmail.com>
2017-08-09 23:01:10 +0300

    added Profile component. try to configure the correct receiving token

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import './style.css';
+import './../style.css';
 import {
   FormGroup,
   FormControl,
   InputGroup,
   Glyphicon
 } from 'react-bootstrap';
-
+import Profile from './../Profile';
 
 class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    query: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      artist: null
+    }
   }
-}
 
-search() {
-  console.log(this.state);
-  const BASE_URL = 'https://api.spotify.com/v1/search?';
-  const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-  console.log(FETCH_URL);
-}
+  search() {
+    console.log(this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log(FETCH_URL);
+    let accessToken = 'BQB-zI1uK2wJK8qbrQPHOm7-JfK3gD5iVdiqoXCNJbwSuDpmhvSpwc0LBJY-VY475hho5fVrzKTM7Mp9vtoEMTxxchxWOq4JeuoesNiQwu9Wh8KYvylWORSLDFT3mIgsRp6Guza23RKHBNljVnAEUoySQPS3kmcVhqYA-JsV3xUxKeO2dIU';
+    fetch(FETCH_URL, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        const artist = json.artists.items[0];
+        this.setState({ artist });
+      });
+  }
 
   render() {
     return (
@@ -33,7 +49,7 @@ search() {
               type="text"
               placeholder="Search for an Artist"
               value={this.state.query}
-              onChange={event => {this.setState({query: event.target.value})}}
+              onChange={event => { this.setState({ query: event.target.value }) }}
               onKeyPress={event => {
                 if (event.key === 'Enter') {
                   this.search()
@@ -41,15 +57,14 @@ search() {
               }}
             />
             <InputGroup.Addon onClick={() => this.search()}>
-            <Glyphicon glyph="search"></Glyphicon>
+              <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
 
         </FormGroup>
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
+        <Profile
+          artist={this.state.artist}
+        />
         <div className="Gallery">
           Gallery
         </div>
---
+++ src/components/Profile/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import './../style.css';
+
+
+class Profile extends Component {
+
+  render() {
+    let artist = {
+      name: '',
+      followers: { total: '' },
+      images: [{ url: '' }],
+      genres: []
+    };
+    artist = this.props.artist !== null ? this.props.artist : artist;
+
+    return (
+      <div className="Profile">
+        <img className="profile-img" src={artist.images[0].url} alt="Profile image" />
+        <div>{artist.name}</div>
+        <div>{artist.followers.total}</div>
+        <div>
+          {
+            artist.genres.map((genre, k) => {
+              genre = genre !== artist.genres[artist.genres.length-1] ? ` ${genre},` : ` & ${genre}`;
+              return (
+                <span key={k}>{genre} </span>
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
