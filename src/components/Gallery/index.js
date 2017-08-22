209990200d9184dc64ae25ca7b975e0bac1e1a49
Andrey Barchuk <ndreybar@gmail.com>
2017-08-22 22:27:41 +0300

    added the play button only to tracks that you can listen

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './../style.css';
 
 
+
 class Gallery extends Component {
   constructor(props) {
     super(props);
@@ -11,32 +12,35 @@ class Gallery extends Component {
       playing: false
     }
   }
-  
+
   playTrack(previewUrl) {
-    let audio = new Audio(previewUrl);
-    if (!this.state.playing) {
-      audio.play();
-      this.setState({
-        playing: true,
-        playingUrl: previewUrl,
-        audio
-      })
-    } else {
-      if (this.state.playingUrl === previewUrl) {
-        this.state.audio.pause();
-        this.setState({
-          playing: false
-        })
-      } else {
-        this.state.audio.pause();
+    if (previewUrl !== null) {
+      let audio = new Audio(previewUrl);
+      if (!this.state.playing) {
         audio.play();
         this.setState({
           playing: true,
           playingUrl: previewUrl,
           audio
-        });
+        })
+      } else {
+        if (this.state.playingUrl === previewUrl) {
+          this.state.audio.pause();
+          this.setState({
+            playing: false
+          })
+        } else {
+          this.state.audio.pause();
+          audio.play();
+          this.setState({
+            playing: true,
+            playingUrl: previewUrl,
+            audio
+          });
+        }
       }
     }
+
   }
 
   render() {
@@ -47,9 +51,22 @@ class Gallery extends Component {
       <div>
         {tracks.map((track, i) => {
           const trackImg = track.album.images[0].url;
+          console.log(track.preview_url);
+          const trackPreview = track.preview_url ? track.preview_url : null;
           return (
-            <div key={i} className="track" onClick={() => this.playTrack(track.preview_url)}>
+
+            <div key={i} className="track" onClick={() => this.playTrack(trackPreview)}>
               <img src={trackImg} className="track-img" alt="track" />
+              <div className="track-play">
+                {
+                  track.preview_url !== null
+                    ? <div className="track-play-inner">
+                      
+                    </div>
+                    : <div></div>
+                }
+
+              </div>
               <p className="track-text">{track.name}</p>
             </div>
           )
